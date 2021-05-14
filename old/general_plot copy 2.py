bf8fddef8aaca512b6d6362cd6158dcb4adbb4ee
almajni <almajni@kth.se>
2021-05-15 01:12:49 +0200

    done with simple cov

--- old/general_plot copy 2.py
+++ old/general_plot copy 2.py
@@ -77,7 +77,6 @@ def set_subplot_extra_options(ax, d_map, ax_t):
     ax.set_ylabel(f"{d_map[f'y_{ax_t}_label']}")
 #     ax
     ax.grid(True)
-    ax.set_xlim([0,d_map[f'x_{ax_t}_lim']])
 
 #     ax.plt.ylim()([0, 100])
 #     ax.set_aspect('equal', adjustable='box')
@@ -91,36 +90,45 @@ def set_subplot_extra_options(ax, d_map, ax_t):
 def prepare_sub_plot(ax, csv_f, x_name, y_name, unique_list, data_map, graph_type):
     all_data = extract_unique_as_array(csv_f, unique_list, x_name, y_name)
     labels = unique_list
-    if data_map['need_positions'] == True and graph_type == 'rand':
-        # pos = np.logspace(1,5, len(unique_list))
+    if data_map['need_positions'] == True:
+        # pos = np.logspace(labels) #np.log(labels)
+        pos = np.logspace(1,5, len(unique_list))
+        w = 0.1
+        # b = 10
+        # width = lambda p, w: 7**(np.log10(p)+w/2.)-7**(np.log10(p)-w/2.)
 
         bplot1 = ax.boxplot(all_data,
                         # positions=[1, 2, 4, 6, 9, 12, 16, 20],  #pos,
                         # positions=[1, 2, 3.5, 6, 9, 13, 17.5, 22.5],  #pos,
                         positions=data_map['positions'],
-                        widths= [data_map[f'box_width_{graph_type}']]*len(unique_list), #pos, #width(pos,w),
+                        widths= [.6]*len(unique_list), #pos, #width(pos,w),
                         labels=labels)
         
+
+        # ax.set_xscale("log")
+        # ax.xticks([0, 1, 2], ['January', 'February', 'March'], rotation=45)  #
+        # ax.set_xticks(np.log(unique_list))
+        ax.set_xticklabels(unique_list, rotation=40)
+
 
     else:
         pos = labels
 
         bplot1 = ax.boxplot(all_data,
-                        labels=labels)  
-
-    if data_map['need_xtick'] == True:
-        ax.set_xticklabels(unique_list, rotation=40)
-
+                        positions=pos,
+                        labels=labels)  # will be used to label x-ticks
 
     set_subplot_extra_options(ax, data_map, graph_type)
 
+#     ymin, ymax = plt.ylim()
+#     plt.ylim( 0.5, ymax * 0.5)
 
 
 def compare_bes_rand_given_y(bes_csv, rand_csv, bes_x_name, rand_x_name, y_common,
                              bes_unique_list, rand_unique_list, data_map):
 
     fig, axs = plt.subplots(nrows=1, ncols=2, sharex=False, sharey=False, squeeze=False, figsize=(
-        data_map['fig_width'], 5), gridspec_kw={'width_ratios': [data_map['width_bes_ratio'], data_map['width_rand_ratio']]})
+        14, 5), gridspec_kw={'width_ratios': [data_map['width_bes_ratio'], data_map['width_rand_ratio']]})
 
 #     plt.gca().set_aspect('equal', adjustable='box')
 
@@ -129,7 +137,7 @@ def compare_bes_rand_given_y(bes_csv, rand_csv, bes_x_name, rand_x_name, y_commo
     prepare_sub_plot(ax1, bes_csv, bes_x_name, y_common,
                      bes_unique_list, data_map, 'bes')
     prepare_sub_plot(ax2, rand_csv, rand_x_name, y_common,
-                     rand_unique_list, data_map, 'rand')
+                     rand_unique_list, data_map, 'random')
 
     fig.suptitle(
         f"BES VS RS in terms of {data_map['y_bes_label']} for {data_map['model_name']}", y=data_map['distance_to_figures'])
@@ -140,9 +148,9 @@ def compare_bes_rand_given_y(bes_csv, rand_csv, bes_x_name, rand_x_name, y_commo
     plt.show()
     img_path = os.path.join(img_general_path, f"{data_map['model_name']}")
     os.makedirs(img_path, exist_ok=True)
-    fig.savefig(os.path.join(img_path,
-                        f"{data_map['y_bes_label']}_bes_rand_{data_map['model_name']}_{data_map['loopOpt']}.svg"),
-                        format="svg")
+#     fig.savefig(os.path.join(img_path,
+#                         f"{data_map['y_bes_label']}_bes_rand_{data_map['model_name']}_{data_map['loopOpt']}.svg"),
+#                         format="svg")
 
 
 def get_csv_unique(model_name, loopOpt):
