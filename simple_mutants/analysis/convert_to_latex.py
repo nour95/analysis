b2b93daa580cb2b407bc45613f25eb0810c8617a
almajni <almajni@kth.se>
2021-05-16 18:21:02 +0200

    done converting to latex

---
+++ simple_mutants/analysis/convert_to_latex.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+
+def to_latex(model_name):
+    latex_dir = os.path.join("latex", model_name)
+    os.makedirs(latex_dir, exist_ok=True)
+
+    all_images = next(os.walk(f'images/{model_name}'))[2] 
+    # print(all_images)
+
+    for image_file in all_images:
+
+        subprocess.run(['inkscape', '-D', f'images/{model_name}/{image_file}', 
+                        '-o',  f'{latex_dir}/{image_file[:-4]}.pdf', 
+                        '--export-latex'])
+
+
+model_name = 'ElevatorModel'
+to_latex(model_name)
+print(f"Done with {model_name}")
+
+model_name = 'JavaNioServerSocket3'
+to_latex(model_name)
+print(f"Done with {model_name}")
+
+model_name = 'SimpleModel'
+to_latex(model_name)
+print(f"Done with {model_name}")
