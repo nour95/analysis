bf586a8217b80fe58060fd94eb62955f81ce05bc
almajni <almajni@kth.se>
2021-05-18 19:49:36 +0200

    Add trie vs actual for list cov

---
+++ list_cov/analysis/convert_to_latex.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+
+def to_latex(model_name):
+    latex_dir = os.path.join("latex", model_name)
+    os.makedirs(latex_dir, exist_ok=True)
+
+    all_images = next(os.walk(f'images/{model_name}'))[2] 
+    # print(all_images)
+
+    for image_file in all_images:
+
+        subprocess.run(['inkscape', '-D', f'images/{model_name}/{image_file}', 
+                        '-o',  f'{latex_dir}/{image_file[:-4]}.pdf', 
+                        '--export-latex'])
+
+
+model_name = 'Array'
+to_latex(model_name)
+print(f"Done with {model_name}")
+
+model_name = 'Linked'
+to_latex(model_name)
+print(f"Done with {model_name}")
